6db370469cbd1167a1548f967bf8568a35161fc6
Noah Feder <noah.feder@gmail.com>
2018-04-28 04:28:20 -0400

    add images, meme_data, and move helpers to separate file

---
+++ meme_data.js
@@ -0,0 +1,11 @@
+var meme_data = {
+  boyfriend: {
+    field_count: 3,
+    field_positions: [
+      [0.28, 0.24],
+      [0.54, 0.19],
+      [0.81, 0.30]
+    ],
+    path: 'img/boyfriend.jpg'
+  }
+}
